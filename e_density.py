67ed42424e0b2d5c9cd3b3655b115631b82c8bbf
Andrei Berceanu <andreicberceanu@gmail.com>
2020-11-25 10:48:40 +0200

    plot 3d example

--- e_density.py
+++ e_density.py
@@ -19,31 +19,32 @@ n_c = laser.ncrit.to_value("1/m**3")
 # 1.7419595910637713e+27
 
 
-p = pathlib.Path.cwd() / "betatron0007" / "simOutput" / "h5"
+p = pathlib.Path.cwd() / "example-3d" / "hdf5"
 ts = OpenPMDTimeSeries(p)
 
 
 rho, rho_info = ts.get_field(
-    field="e_density",
+    field="rho",
     coord=None,
-    iteration=50000,
+    iteration=500,
     theta=None,
-    slice_across="z",
+    slice_across="y",
 )
 
 fig, ax = pyplot.subplots(figsize=(7, 5))
 
 # Plot the data
 im = ax.imshow(
-    np.rot90(rho / n_c),
-    extent=np.roll(rho_info.imshow_extent * 1e6, 2),
+    rho,
+    extent=rho_info.imshow_extent * 1e6,
     interpolation="nearest",
+    origin="lower",
     aspect="auto",
     norm=colors.SymLogNorm(linthresh=1e-4, linscale=0.15, base=10),
 )
 # Add the name of the axes
-ax.set_ylabel("$%s \;(\mu m)$" % rho_info.axes[1])
-ax.set_xlabel("$%s \;(\mu m)$" % rho_info.axes[0])
+ax.set_xlabel("$%s \;(\mu m)$" % rho_info.axes[1])
+ax.set_ylabel("$%s \;(\mu m)$" % rho_info.axes[0])
 
 fig.colorbar(im)
-fig.savefig("e_density_z.png")
+fig.savefig("e_density.png")
